75f96c0f9724501313efec7c2ed26b330d5f9feb
Rohan Maharjan <Rohan9841@gmail.com>
2018-12-25 20:58:14 -0600

    Add files via upload
    Almost done

--- ObjectSvg.js
+++ ObjectSvg.js
@@ -1,35 +1,101 @@
-class Chart{
+class Chart {
 
     //this will be called by our index.html
-    constructor(opts)
-    {   
+    constructor(opts) {
         //this is the element where our rect will be appeneded
         this.element = opts.element;
 
         //position to display our rect
-        this.pos = opts.pos;
+        this.dataset = opts.cutPoints;
+
+        //phage that is cut
+        this.phage = opts.phage;
 
         //calling drawRect() function
         this.drawRect();
     }
 
-    drawRect(){
+    drawRect() {
+        console.log(this.dataset);
+        //declare svgWidth
+        this.width = $(".svgDiv").width();
+        console.log(this.width);
+        this.height = $(".svgDiv").height()/1.5;
+        this.padding = (1.95/100)*1028;
+        var that = this;
 
-        //setting the width, padding and height of our rectangle
-        var padding = 20;
-        this.width = $("#container").width();
-        this.height = 100;
+        //radius of circle
+        this.radius = (0.4 / 100) * this.width;
 
-        //appending rectangles to the element i.e container
-        var rect = d3.select(this.element)
-                    .append('rect')
-                    .attr("x",0) //setting x-coordinate
-                    .attr("y",this.pos)//setting y-coordinate
-                    .attr("width",this.width-padding)
-                    .attr("height",this.height)
-                    .attr("fill","none")
-                    .style("stroke","black") //creating border
-                    .style("stroke-width",1.5); //setting border width
+        //declare properties of svg
+        this.svg = d3.select(this.element)
+            .append("svg")
+            .attr("width", this.width)
+            .attr("height", this.height)
+            .call(d3.zoom()
+                .extent([[this.padding, 0], [this.width - this.padding, 0]])
+                .scaleExtent([1, 100])
+                .translateExtent([[this.padding, 0], [this.width - this.padding, 0]])
+                .on("zoom", zoomed));
+
+        //scaling the domain and range 
+        this.xScale = d3.scaleLinear()
+            .domain([0, d3.max(this.dataset)])
+            .range([this.padding, this.width - this.padding]);
+
+        //creating x-axis
+        this.x_axis = d3.axisBottom(this.xScale);
+
+        //position of the x-axis in height
+        this.xAxisTranslate = this.height / 2;
+
+        //calling x-axix        
+        this.gX = this.svg.append("g")
+            .attr("transform", "translate(0," + this.xAxisTranslate + ")")
+            .call(this.x_axis);
+
+        //Add circle to the line
+        this.circ = this.svg.selectAll("circle")
+            .data(this.dataset)
+            .enter()
+            .append("circle")
+            .attr("class", "cir")
+            .attr("cx", function (d) { return (that.xScale(d)); })
+            .attr("cy", this.xAxisTranslate)
+            .attr("r", this.radius)
+            .attr("fill", "purple")
+            .on("mouseover", function (d) {
+                d3.select(this)
+                    .transition()
+                    .duration(50)
+                    .style("cursor", "pointer")
+                    .attr("r", that.radius / 0.75)
+            })
+
+            .on("mouseout", function (d) {
+                d3.select(this)
+                    .transition()
+                    .duration(50)
+                    .style("cursor", "normal")
+                    .attr("r", that.radius)
+            })
+            .append("title")
+            .text((d) => d)
+            .text();
+
+        function zoomed() {
+            //create new scale objects based on event
+            that.new_xScale = d3.event.transform.rescaleX(that.xScale);
+
+            //update axes
+            that.gX.call(that.x_axis.scale(that.new_xScale));
+            that.svg.selectAll("circle").data(that.dataset)
+                .attr('cx', function (d) { return that.new_xScale(d) })
+                .attr('cy', that.xAxisTranslate);
+        }
+
+        $('<p>'+this.phage+' is cut '+this.dataset.length+' times</p>').appendTo(this.element);
+
     }
 
 }
